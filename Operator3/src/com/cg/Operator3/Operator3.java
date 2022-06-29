c1a1dc861c71ddf1f56c5d89737ae87af823abe4
pc <pc@DESKTOP-9VMHA9D>
2022-06-29 07:48:41 -0700

    first commit

---
+++ Operator3/src/com/cg/Operator3/Operator3.java
@@ -0,0 +1,11 @@
+package com.cg.Operator3;
+public class Operator3 {
+public static void main(String[] args){
+		int var1=5;
+		int var2=6;
+		if((var2=1)==var1);
+		System.out.println(var2);
+	
+			System.out.println(++var2);
+	}
+}
