37809c7f42c7942fce0ba0366935acd27fffd136
pc <pc@DESKTOP-9VMHA9D>
2022-06-29 07:45:14 -0700

    first commit

---
+++ Operator2/src/com/cg/operator1/Operator1.java
@@ -0,0 +1,17 @@
+package com.cg.operator1;
+
+public class Operator1 {
+
+	public static void main(String[] args) {
+		int a=5,b=18;
+		if((a=3)==b)
+		{
+		System.out.println(++a);
+		}
+		else
+		{
+			System.out.println(++b);
+		}
+	}
+
+}
