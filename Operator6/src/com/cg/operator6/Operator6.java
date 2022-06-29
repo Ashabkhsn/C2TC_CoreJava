f7d61c5931c941fba3da1390c3919d40c91b4ecf
pc <pc@DESKTOP-9VMHA9D>
2022-06-29 07:50:57 -0700

    first commit

---
+++ Operator6/src/com/cg/operator6/Operator6.java
@@ -0,0 +1,12 @@
+package com.cg.operator6;
+
+public class Operator6 {
+
+	public static void main(String[] args) {
+		System.out.print("Y"+"O");
+		System.out.print('L');
+		System.out.print('O');
+		
+	}
+
+}
