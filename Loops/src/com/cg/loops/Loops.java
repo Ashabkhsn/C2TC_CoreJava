04722924fbe5f79a02d2ba9962642762c7e69150
pc <pc@DESKTOP-9VMHA9D>
2022-07-01 07:52:29 -0700

    first commit

---
+++ Loops/src/com/cg/loops/Loops.java
@@ -0,0 +1,18 @@
+package com.cg.loops;
+
+public class Loops {
+
+	public static void main(String[] args) {
+		String s ="friends";
+		int x=0;
+		do {
+			System.out.println(s.charAt(x));
+			x++;
+			
+			
+		}while(x<2);
+		
+
+	}
+
+}
---
+++ Loops/src/com/cg/loops/Loops1.java
@@ -0,0 +1,5 @@
+package com.cg.loops;
+
+public class Loops1 {
+
+}
