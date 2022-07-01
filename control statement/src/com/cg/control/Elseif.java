93ec2a5a5097819c6050a1be34bbeea677a9c972
pc <pc@DESKTOP-9VMHA9D>
2022-07-01 07:14:28 -0700

    first commit

---
+++ control statement/src/com/cg/control/Elseif.java
@@ -0,0 +1,20 @@
+package com.cg.control;
+
+public class Elseif {
+
+	public static void main(String[] args) {
+		String lang = "java";
+		if(lang == "python") {
+			System.out.println("language is python");
+			
+		}else if(lang == "c++") {
+			System.out.println("lang is c++");
+			
+		}else if(lang =="java") {
+			System.out.println("lang is java");
+			}else {
+				System.out.println(lang);
+			}
+		}
+
+}
